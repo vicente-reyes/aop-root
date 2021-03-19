5a0cf0b16427f184909d09e4d9fc4b72e347fb2c
vicente-reyes <vicentereyescarvallo@gmail.com>
2021-03-18 20:46:53 -0600

    creacion de pages, sections, includes

---
+++ app/Views/include/header.php
@@ -0,0 +1,19 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top header-transparent">
+    <div class="container d-flex align-items-center">
+        <h1 class="logo mr-auto"><a href="index.html">A&O P.</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        <nav class="nav-menu d-none d-lg-block">
+            <ul>
+                <li class="active"><a href="<?php echo base_url() ?>">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#pricing">Pricing</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -5,8 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <?= $this->include('include/link') ?>    
 </head>
 <body>
     
+    <?= $this->include('include/header') ?>
+
+    <?= $this->include('include/section/hero') ?>
+
+    <main id="main">
+
+    <?= $this->include('include/section/about') ?>
+
+    <?= $this->include('include/section/team') ?>
+
+    <?= $this->include('include/section/counts') ?>
+
+    <?= $this->include('include/section/contact') ?>
+
+
+
+
+    
+
+    
+
+    </main>
+    
+    <?= $this->include('include/footer') ?>
+
+    <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+
+    <?= $this->include('include/js') ?>
+
 </body>
 </html>
